f04da9c74a58fd6a8264a0829131d33a62df59f4
trangvan <trang@akocommerce.com>
2023-07-25 17:16:21 +0700

    link the products received to the website

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { storefront } from './utils/index.js'
 
 const staticProducts = [
@@ -196,29 +197,35 @@ export default function Home({products}) {
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">Our products</h2>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {staticProducts.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+          {products.edges.map((item) => {
+
+            const product = item.node
+            const image = product.images.edges[0].node
+
+            return (
+              <Link key={product.handle}  href={`/products/${product.handle}`}>
+                <div className="group relative">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                  <img
+                    src={image.url}
+                    alt={image.altText}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
                 </div>
-                <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {product.title}
+                    </h3>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">{product.priceRange.minVariantPrice.amount}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            </Link>
+            )
+          }
+          )}
         </div>
       </div>
     </main>
